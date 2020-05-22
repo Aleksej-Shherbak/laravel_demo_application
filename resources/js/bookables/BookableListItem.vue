0bbe2d58bf642ff6b773cb6d32f460cc595fe751
aleksej <aleksej.shherbak@yandex.ru>
2020-05-23 02:03:54 +0300

    Added nice grid to Bookables.vue component

--- resources/js/bookables/BookableListItem.vue
+++ resources/js/bookables/BookableListItem.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
-        <h1>{{ title }}</h1>
-        <p>{{ content }}</p>
-        <p>price {{ price }}</p>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{ title }}</h5>
+            <p class="card-text">{{ content }}</p>
+            <p class="card-text">price {{ price }}</p>
+        </div>
+
     </div>
 </template>
 
--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -4,12 +4,18 @@
             Loading ...
         </div>
 
-        <bookable-list-item v-for="(bookable, index) in bookables"
-            :title="bookable.title"
-            :content="bookable.content"
-            :price="bookable.price"
-            :key="index">
-        </bookable-list-item>
+        <div class="row mb-4" v-for="row in rows" :key="'row'+row">
+            <div class="col" v-for="(bookable, column) in bookableInRow(row)"
+                 :key="'row' + row + column">
+                <bookable-list-item
+                    :title="bookable.title"
+                    :content="bookable.content"
+                    :price="bookable.price">
+                </bookable-list-item>
+            </div>
+            <div class="col" v-for="p in placeholdersInRow(row)" :key="'placeholder' + row + p"></div>
+        </div>
+
     </div>
 </template>
 
@@ -27,22 +33,61 @@
                 this.bookables = [
                     {
                         title: "Expensive villa 1",
-                        content: "This is pretty expensive vile 1",
-                        price: "2000$"
+                        content: "This is pretty expensive villa 1",
+                        price: 2000
                     },
                     {
                         title: "Expensive villa 2",
-                        content: "This is very expensive vile 2",
-                        price: "5000$"
+                        content: "This is very expensive villa 2",
+                        price: 5000
+                    },
+                    {
+                        title: "Expensive villa 3",
+                        content: "This is pretty expensive villa 3",
+                        price: 2000
+                    },
+                    {
+                        title: "Expensive villa 4",
+                        content: "This is very expensive villa 4",
+                        price: 5000
+                    },
+                    {
+                        title: "Expensive villa 5",
+                        content: "This is pretty expensive villa 5",
+                        price: 2000
+                    },
+                    {
+                        title: "Expensive villa 6",
+                        content: "This is very expensive villa 6",
+                        price: 5000
+                    },
+                    {
+                        title: "Expensive villa 7",
+                        content: "This is very expensive villa 7",
+                        price: 5000
                     },
                 ];
                 this.loading = false;
             }, 3000);
         },
+        computed: {
+            rows() {
+                return Math.ceil(this.bookables.length / this.columns);
+            }
+        },
+        methods:  {
+            bookableInRow : function(row) {
+                return this.bookables.slice((row - 1) * this.columns, row * this.columns)
+            },
+            placeholdersInRow: function (row) {
+                return  this.columns - this.bookableInRow(row).length
+            }
+        },
         data: function () {
             return {
                 bookables: [],
                 loading: false,
+                columns: 3
             }
         }
     }
