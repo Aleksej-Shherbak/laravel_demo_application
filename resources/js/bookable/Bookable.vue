e03f138bafa88e1a4d7ca0b0e7295c214c42d0eb
aleksej <aleksej.shherbak@yandex.ru>
2020-05-24 00:05:48 +0300

    Started Bookable show page (for showing one entity).

---
+++ resources/js/bookable/Bookable.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>Bookable</div>
+</template>
+
+<script>
+    export default {
+        name: "Bookable",
+        created() {
+            axios.get(`/api/bookables/${this.$route.params.id}`)
+            .then(res => {
+                this.bookable = res.data
+            });
+
+        },
+        data() {
+            return {
+                bookable: null,
+            };
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
