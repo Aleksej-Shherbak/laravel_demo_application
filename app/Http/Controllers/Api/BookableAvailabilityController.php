3b427d37c1e3e32d0dc1468e312b5c049e861a9c
aleksej <aleksej.shherbak@yandex.ru>
2020-05-24 23:44:15 +0300

    Added bookable availability checking mechanism

---
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Bookable;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BookableAvailabilityController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param int $id
+     * @param \Illuminate\Http\Request $request
+     * @return void
+     */
+    public function __invoke(int $id, Request $request)
+    {
+        $data = $request->validate([
+            'from' => 'required|date_format:Y-m-d|after_or_equal:now',
+            'to' => 'required|date_format:Y-m-d|after_or_equal:from',
+        ]);
+
+        $bookable = Bookable::findOrFail($id);
+
+        return $bookable->availableFor($data['from'], $data['to'])
+            ? response()->json([])
+            : response()->json([], 404);
+
+    }
+}
