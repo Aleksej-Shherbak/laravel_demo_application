53dde0de065f562c7a946d0230f19f2008b4012f
aleksej <aleksej.shherbak@yandex.ru>
2020-05-24 19:51:47 +0300

    Added Booking model and started UI for that, added laravel-debug tool

---
+++ resources/js/bookable/Availability.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <h6 class="text-uppercase text-secondary font-weight-bold">Check Availability</h6>
+
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="from">From</label>
+                <input type="text" id="from" name="from"
+                       class="form-control form-control-sm"
+                       placeholder="Start date" v-model="from"
+                    @keyup.enter="check">
+            </div>
+
+            <div class="form-group col-md-6">
+                <label for="to">To</label>
+                <input type="text" id="to" name="to"
+                       class="form-control form-control-sm"
+                       placeholder="End date" v-model="to"
+                       @keyup.enter="check">
+            </div>
+
+            <button class="btn btn-secondary btn-block" @click.prevent="check">Check!</button>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Availability",
+        data() {
+            return {
+                from: null,
+                to: null
+            }
+        },
+        methods: {
+            check() {
+                alert("test");
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    label {
+        font-size: 0.7rem;
+        text-transform: uppercase;
+        color: gray;
+        font-weight: bolder;
+    }
+</style>
--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-8 pb-4">
             <div class="card">
                 <div class="card-body">
                     <template v-if="!loading">
@@ -12,15 +12,20 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
-            availability and price are here ...
+        <div class="col-md-4 pb-4">
+            <availability></availability>
         </div>
     </div>
 </template>
 
 <script>
+    import Availability from "./Availability";
+
     export default {
         name: "Bookable",
+        components: {
+            Availability
+        },
         created() {
             this.loading = true;
             axios.get(`/api/bookables/${this.$route.params.id}`)
