8619381b5dc8b12bf1a72cddf2cb2c9965ccc9aa
aleksej <aleksej.shherbak@yandex.ru>
2020-07-14 00:13:55 +0300

    Updated Dockerfile fro php-fpm, added not root user

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -6,7 +6,7 @@
         </div>
         <div class="form-group">
             <label for="content" class="text-muted">Describe your experience with</label>
-            <textarea name="" id="content" cols="30" rows="10" class="form-control"></textarea>
+            <textarea v-model="review.content" id="content" cols="30" rows="10" class="form-control"></textarea>
         </div>
         <button class="btn btn-primary btn-lg btn-block">Submit</button>
     </div>
@@ -18,6 +18,12 @@
         name: "Review",
         components: {StarRating},
         methods: {
+
+        },
+        created() {
+            // Check if review already exists
+            // Fetch a booking ny a review key
+            // Store the review
 
         },
         data() {
