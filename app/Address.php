8ae88459647cacca55c18ed34e238d08c694eb9d
aleksej <aleksej.shherbak@yandex.ru>
2020-07-27 00:01:39 +0300

    Added Address.php model and migrations

---
+++ app/Address.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Address
+ * @package App
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $email
+ * @property string $street
+ * @property string $city
+ * @property string $country
+ * @property string $state
+ * @property string $zip
+ * @property integer $id
+ */
+class Address extends Model
+{
+    //
+}
--- resources/js/basket/Basket.vue
+++ resources/js/basket/Basket.vue
@@ -1,6 +1,56 @@
 <template>
     <div class="row">
-        <div class="col-md-8"> Checkout form</div>
+        <div class="col-md-8">
+            <div class="form-row">
+                <div class="col-md-6 form-group">
+                    <label for="first-name">First name</label>
+                    <input type="text" id="first-name" class="form-control">
+                </div>
+                <div class="col-md-6 form-group">
+                    <label for="last-name">Last name</label>
+                    <input type="text" id="last-name" class="form-control">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col-md-12 form-group">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" class="form-control">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col-md-6 form-group">
+                    <label for="street">Street</label>
+                    <input type="text" id="street" class="form-control">
+                </div>
+
+                <div class="col-md-6 form-group">
+                    <label for="city">City</label>
+                    <input type="text" id="city" class="form-control">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col-md-6 form-group">
+                    <label for="country">Country</label>
+                    <input type="text" id="country" class="form-control">
+                </div>
+                <div class="col-md-4 form-group">
+                    <label for="state">State</label>
+                    <input type="text" id="state" class="form-control">
+                </div>
+                <div class="col-md-2 form-group">
+                    <label for="zip-code">Zip code</label>
+                    <input type="text" id="zip-code" class="form-control">
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-12 form-group">
+                    <button type="submit" class="btn btn-block btn-lg btn-primary">
+                        Book now!
+                    </button>
+                </div>
+            </div>
+        </div>
         <div class="col-md-4">
             <div class="d-flex justify-content-between">
                 <h6 class="text-uppercase text-secondary font-weight-bold">Your Cart</h6>
@@ -34,7 +84,6 @@
                             <i class="fa fa-trash-alt"></i>
                         </button>
                     </div>
-
                 </div>
             </transition-group>
 
